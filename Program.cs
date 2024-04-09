e4b4f99c64f9b0984d3a017c7366a00ad34232ab
Leifeng2000 <leifeng7112@gmail.com>
2024-04-09 19:50:11 +0700

    FindArray
    Array

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] myStudent = {"Mai", "dung", "Minh", "Linh", "Hieu", "Toan", "Hung"};
+        Console.WriteLine("Nhap 1 cai ten tu ban phim");
+        string name = Console.ReadLine();
+        bool isHas = false;
+        for (int i = 0; i < myStudent.Length; i++)
+        {
+            if (myStudent[i].Equals(name))
+            {
+                Console.WriteLine("Co sinh vien o trong lop");
+                isHas = true;
+                break;
+            }
+        }
+        if (!isHas)
+        {
+            Console.WriteLine("Khong co sinh vien trong lop");
+        }
+    }
+}
